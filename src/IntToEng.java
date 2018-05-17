b2c26fc87d62ca10a23a8ddba949042d2f8e7637
g16922kc <g16922kc@s30405.tsuda.ac.jp>
2018-05-17 14:12:46 +0900

    31~50

--- src/IntToEng.java
+++ src/IntToEng.java
@@ -25,6 +25,32 @@ public class IntToEng {
     	if(n==8) return "eight";
     	if(n==9) return "nine";
     	if(n==10) return "ten";
+    	if(n==11) return "eleven";
+    	if(n==12) return "twelve";
+    	if(n==13) return "thirteen";
+    	if(n==14) return "fourteen";
+    	if(n==15) return "fifteen";
+    	if(n==16) return "sixteen";
+    	if(n==17) return "seventeen";
+    	if(n==18) return "eighteen";
+    	if(n==19) return "nineteen";	
+    	if(n==20) return "twenty";
+    	
+    	String a[] = {"","one","two","three","four","five","six","seven","eight","nine"}; 
+    	if(n>=21 && n<30){
+    		int A=n-20;
+    		return "twenty "+a[A];
+    	}
+    	if(n==30) return "thirty";
+    	if(n>=31 && n<40){
+    		int A=n-30;
+    		return "thirty "+a[A];
+    	}
+    	if(n==40) return "fourty";
+    	if(n>=41 && n<50){
+    		int A=n-40;
+    		return "fourty "+a[A];
+    	}
     	return "";
     }
 }
